ffa5874037d2175d6375db56c99c44287acd70ef
Baluk Pavel <xpomocoma001@outlook.com>
2021-09-28 00:32:59 +0300

    sword

---
+++ Assets/__Scripts/InRoom.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InRoom : MonoBehaviour
+{
+    public static float ROOM_W = 16;
+    public static float ROOM_H = 11;
+    public static float WALL_T = 2;
+
+    public Vector2 roomPos
+    {
+        get
+        {
+            Vector2 tPos = transform.position;
+            tPos.x %= ROOM_W;
+            tPos.y %= ROOM_H;
+            return tPos;
+        }
+        set
+        {
+            Vector2 rm = roomNum;
+            rm.x *= ROOM_W;
+            rm.y *= ROOM_H;
+            rm += value;
+            transform.position = rm;
+        }
+    }
+
+    public Vector2 roomNum
+    {
+        get
+        {
+            Vector2 tPos = transform.position;
+            tPos.x = Mathf.Floor(tPos.x / ROOM_W);
+            tPos.y = Mathf.Floor(tPos.y / ROOM_H);
+            return tPos;
+        }
+        set
+        {
+            Vector2 rPos = roomPos;
+            Vector2 rm = value;
+            rm.x *= ROOM_W;
+            rm.y *= ROOM_H;
+            transform.position = rm + rPos;
+        }
+    }
+}
